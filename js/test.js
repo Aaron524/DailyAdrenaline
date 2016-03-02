20b286e28041842e47f510cd221994721525d2b3
Aaron Davis <ajdavis425@gmail.com>
2016-03-02 14:21:36 -0500

    Creating of repo for DailyAdrenaline

---
+++ js/test.js
@@ -0,0 +1,8 @@
+var x = 5;         // assign the value 5 to x
+var y = 2;         // assign the value 2 to y
+var z = x + y;     // assign the value 7 to z (x + y)
+
+console.log(x+2)
+x=5+10
+
+console.log(x+10)
